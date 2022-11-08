675481362a6ac3b2f7699d724d29e30d5081be58
Yurii Kinakh <yikinakh@thevirtugroup.com>
2022-11-08 18:31:29 +0200

    - db setup

---
+++ src/main/java/model/User.java
@@ -0,0 +1,14 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+}
