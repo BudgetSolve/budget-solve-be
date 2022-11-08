3636d9af1e16a773176e1cf340016ce5cb856961
Yurii Kinakh <yikinakh@thevirtugroup.com>
2022-11-08 17:47:16 +0200

    - initial

---
+++ src/main/java/budgetsolve/main/BudgetSolveApplication.java
@@ -0,0 +1,13 @@
+package budgetsolve.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetSolveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetSolveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/budgetsolve/main/BudgetSolveApplicationTests.java
@@ -0,0 +1,13 @@
+package budgetsolve.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetSolveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
